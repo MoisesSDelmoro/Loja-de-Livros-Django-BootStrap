ce7d5296dab7b9b796c8eb86cd61fc55cb79074f
moisessdelmoro <moisesdelmoro@gec.inatel.br>
2021-05-27 15:12:53 -0300

    serializers

--- livros/views.py
+++ livros/views.py
@@ -1,14 +1,25 @@
-from livros.apps import LivrosConfig
-from django.http.response import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Livro
 
+@api_view(['GET', 'POST'])
 def livros_view(request):
-    livros = Livro.objects.all()
-    output = [{
-        'nome': livros.nome,
-        'categoria': livros.categoria,
-        'autor': livros.autor
-    } for livro in  livros]
+    if request.method == 'GET':
+        livros = Livro.objects.all()
+        output = [{
+            'nome': livro.nome,
+            'categoria': livro.categoria,
+            'autor': livro.autor
+        } for livro in  livros]
+    elif request.method == 'POST':
+        livro = Livro.objects.create(nome=request.data.get('nome'),
+                                    categoria=request.data.get('categoria'),
+                                    autor=request.data.get('autor'))
+        output = [{
+            'nome': livro.nome,
+            'categoria': livro.categoria,
+            'autor': livro.autor
+        }]
 
-    return HttpResponse(str(output))
+    return Response(output)
 
--- lojaDeLivros/settings.py
+++ lojaDeLivros/settings.py
@@ -19,10 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$d_ipl%()qpl2e4&8=f912yc^v70_iq!qxv83tun7&omey2jgq'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -38,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'livros',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
